add0564a6aadfa204f30d2e3706f7e2e5340939d
murillotlopes <murillo.tlopes@hotmail.com>
2022-04-02 10:51:48 -0300

    update e delete de glossary

--- backend/controllers/glossaryController.js
+++ backend/controllers/glossaryController.js
@@ -27,4 +27,52 @@ controller.retrieve = async (req, res) => {
     }
 }
 
+controller.retrieveOne = async (req, res) => {
+    try{
+        const id = req.params.id
+        const result = await Glossary.findById(id)
+
+        if(result) res.send(result)
+
+        else res.status(404).end()
+
+    }catch(error) {
+        console.log(error)
+
+        res.status(500).send(error)
+    }
+}
+
+controller.update = async (req, res) => {
+    try{
+        const id = req.body._id
+        const result = await Glossary.findByIdAndUpdate(id, req.body)
+
+        if(result) res.status(204).end()
+
+        else res.status(404).end()
+
+    }catch(error) {
+        console.log(error)
+
+        res.status(500).send(error)
+    }
+}
+
+controller.delete = async (req, res) => {
+    try{
+        const id = req.body._id
+        const result = await Glossary.findByIdAndDelete(id)
+
+        if(result) res.status(204).end()
+
+        else res.status(404).end()
+
+    }catch(error) {
+        console.log(error)
+
+        res.status(500).send(error)
+    }
+}
+
 module.exports = controller
--- backend/routes/glossaryRoutes.js
+++ backend/routes/glossaryRoutes.js
@@ -6,5 +6,8 @@ const controller = require('../controllers/glossaryController')
 
 router.post('/', controller.create)
 router.get('/', controller.retrieve)
+router.get('/:id', controller.retrieveOne)
+router.put('/', controller.update)
+router.delete('/', controller.delete)
 
 module.exports = router
